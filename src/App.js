8a4a8bb8e41e5ddd008e3a1236fb4f9c7b74f63f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-17 23:10:23 -0400

    Soluçao dos exercicios de fixaçao 1, 2, 3, 4, 5, 6, 7, 8 e 9 do dia 12.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
+import React, { Component } from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <br></br>
+        <Link to="/about">About</Link>
+        <br></br>
+        <Link to="/users">Users</Link>
+        <br></br>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
+      </BrowserRouter>
     );
   }
 }
